cb39b913f765ad5600cb2b286c3c4b41ba010e84
Gregory Wagonblast <ghw3@pitt.edu>
2026-02-22 14:37:41 -0500

    Extract all evidence from the data file.

--- Filtering.py
+++ Filtering.py
@@ -4,6 +4,9 @@
 
 def filtering(evidence_data_add, prior, total_day):
     # you need to implement this method.
+    
+    # Extract the observations and save to observations list
+    observations = extract(evidence_data_add)
 
     x_prob_rain = []
     # x_prob_sunny[i] = 1 - x_prob_rain[i]
@@ -13,7 +16,41 @@ def filtering(evidence_data_add, prior, total_day):
         
     return x_prob_rain
 
+'''
+Extract the evidence from the data file
 
+Args:
+    file (file object): The file object containing evidence
+
+Returns:
+    observations (list): The list of observations per day
+'''
+def extract(evidence_data_add):
+    # Open file containing sentences
+    with open(evidence_data_add, 'r') as file:
+        observations = parse_file(file)
+
+    return observations
+
+'''
+Parse the evidence file to collect the observations
+
+Args:
+    file (file object): The file object containing evidence
+
+Returns:
+    observations (list): The list of observations per day
+'''
+def parse_file(file):
+    observations = []
+    # For each line in the file, extract truth value based on observation
+    for line in file:
+        parts = line.strip().split('\t')
+        print(parts)
+        obs = parts[1]
+        observations.append(obs == 'take umbrella')
+
+    return observations
 
 
 # following lines are main function:
